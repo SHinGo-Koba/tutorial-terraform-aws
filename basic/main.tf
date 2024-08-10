7a285ba6df94e53a6d519122901109c5933b4b83
SHinGo-Koba <kobayashi.shingo@moneyforward.co.jp>
2024-08-10 15:33:39 +0900

    First commit

---
+++ basic/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    # For AWS provider documentation, see https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.8.0"
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
