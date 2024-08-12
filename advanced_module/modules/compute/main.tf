d0b9efca448ad0c4a42dca4f5caf2401c5b3230d
SHinGo-Koba <kobayashi.shingo@moneyforward.co.jp>
2024-08-12 15:37:46 +0900

    Refactor

--- advanced_module/modules/compute/main.tf
+++ advanced_module/modules/compute/main.tf
@@ -1,16 +1,10 @@
-data "aws_ami" "latest_amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+locals {
+  ami_id = "ami-03d25459ad01ac2b9" # Amazon Linux 2 AMI 64-bit (x86)
 }
 
 resource "aws_launch_template" "plain" {
   name_prefix   = "lt-${var.environment}-"
-  image_id      = data.aws_ami.latest_amazon_linux.id
+  image_id      = local.ami_id
   instance_type = var.instance_type
 
   vpc_security_group_ids = var.security_group_ids
